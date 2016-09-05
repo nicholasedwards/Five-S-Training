bd6aee5a88b41e2e0fbf99f78181355feeae1f0d
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-09-05 16:22:04 -0600

    Green integration spec for static pages

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe 'Static Pages' do
 
   it "has a Home page, an About page, and links between the two" do
   	visit(root_path)
-  	expect(page).to have_selector('about')
-  	expect(page).to have_selector('home')
+  	expect(page).to have_selector('#about')
+    expect(page).to have_selector('#home')
   	click_on('About')
   	expect(page).to have_current_path(about_path)
-  	expect(page).to have_selector('about')
-  	expect(page).to have_selector('home')
+    expect(page).to have_selector('#about')
+    expect(page).to have_selector('#home')
   	click_on('Home')
   	expect(page).to have_current_path(root_path)
   end
