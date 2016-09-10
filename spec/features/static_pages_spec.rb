831dd8b6d6e4919e0743d192454f454272ebdc4b
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-09-10 15:54:41 -0600

    Strengthen integration spec for lesson title display
    Reduce spec dependence on the implementation of lesson title display in the view and controller. Former specs would have passed if only one lesson had its title displayed out of many lessons in the database. Currently, if there are two lessons in the database, they both must be displayed in order for the specs to pass. To my understanding, all lesson titles will be displayed on the home page for any number of lessons in the database.

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Static Pages' do 
 
-  let!(:lesson) { FactoryGirl.create(:lesson) }
+  let!(:lesson_1) { FactoryGirl.create(:lesson, title: "Lesson Factory Title 1") }
+  let!(:lesson_2) { FactoryGirl.create(:lesson, title: "Lesson Factory Title 2") }
 
   it "has a Home page, an About page, and links between the two" do
   	visit(root_path)
   	expect(page).to have_selector('#about')
     expect(page).to have_selector('#home')
     expect(page).to have_title "5S Training | Home"
-    expect(page).to have_content(lesson.title)
+    expect(page).to have_content(lesson_1.title)
+    expect(page).to have_content(lesson_2.title)
   	click_on('About')
   	expect(page).to have_current_path(about_path)
     expect(page).to have_selector('#about')
