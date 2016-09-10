cb156951c19c9a106d8b52b82532f34debeab612
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-09-10 14:57:05 -0600

    Red integration spec for lesson title display
    Specify that lesson titles should be displayed on the home page.

---
+++ spec/factories/lessons.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	
+  factory :lesson do
+  	title "Lesson Factory Title"
+  	content "Lesson Factory Content"
+  end
+
+end
--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Static Pages' do 
 
+  let!(:lesson) { FactoryGirl.create(:lesson) }
+
   it "has a Home page, an About page, and links between the two" do
   	visit(root_path)
   	expect(page).to have_selector('#about')
     expect(page).to have_selector('#home')
     expect(page).to have_title "5S Training | Home"
+    expect(page).to have_content(lesson.title)
   	click_on('About')
   	expect(page).to have_current_path(about_path)
     expect(page).to have_selector('#about')
