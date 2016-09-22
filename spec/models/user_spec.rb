ead0130c041db91746e2f89efbc435488f835e8c
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-09-21 22:31:54 -0600

    Red unit spec for email presence validation
    User validates presence of email attribute

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe User do
     expect(user).to respond_to(:password_digest)
   end
 
+  it "validates presence of email attribute" do
+  	user_no_email = FactoryGirl.build_stubbed(:user, email: nil)
+  	expect(user_no_email).not_to be_valid
+  	
+  	user_no_email = FactoryGirl.build_stubbed(:user, email: " ")
+  	expect(user_no_email).not_to be_valid
+  end
+
 end
