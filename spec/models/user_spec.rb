181cceca0715374d036938b5a358eb12b1054176
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-09-21 23:02:31 -0600

    Red unit spec for email format validation
    User validates for a properly formatted email address

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,9 +11,26 @@ RSpec.describe User do
   it "validates presence of email attribute" do
   	user_no_email = FactoryGirl.build_stubbed(:user, email: nil)
   	expect(user_no_email).not_to be_valid
-  	
+
   	user_no_email = FactoryGirl.build_stubbed(:user, email: " ")
   	expect(user_no_email).not_to be_valid
+  end
+
+  it "validates for a properly formatted email address" do
+    user_invalid_email = FactoryGirl.build_stubbed(:user, email: "invalid")
+    expect(user_invalid_email).not_to be_valid
+
+    user_invalid_email = FactoryGirl.build_stubbed(:user, email: "invalid@")
+    expect(user_invalid_email).not_to be_valid
+
+    user_invalid_email = FactoryGirl.build_stubbed(:user, email: "invalid@com")
+    expect(user_invalid_email).not_to be_valid
+
+    user_invalid_email = FactoryGirl.build_stubbed(:user, email: "invalid@gmail.")
+    expect(user_invalid_email).not_to be_valid
+
+    user_valid_email = FactoryGirl.build_stubbed(:user, email: "valid@gmail.com")
+    expect(user_valid_email).to be_valid
   end
 
 end
