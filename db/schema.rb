1a41c6ac731017a8485e0f5967ac1d8cda2c075d
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-09-21 22:13:13 -0600

    Green unit spec for User model with two attributes
    User has two ActiveRecord attributes: email and password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160910211107) do
+ActiveRecord::Schema.define(version: 20160922041204) do
 
   create_table "lessons", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
