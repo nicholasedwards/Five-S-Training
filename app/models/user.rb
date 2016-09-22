02cd1171211f1179ea941b03e71bbf0ac2ad112b
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-09-21 23:12:42 -0600

    Green unit spec for email format validation
    User validates for a properly formatted email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-	validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-\.]+@[\w\-\.]+\.[a-z]+\z/i	
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
