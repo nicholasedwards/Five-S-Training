051bcf1692f891604b2c8463b7603e84741f718a
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-09-26 10:07:11 -0600

    Green unit spec for email uniqueness validation
    User validates for a unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-\.]+@[\w\-\.]+\.[a-z]+\z/i	
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 end
