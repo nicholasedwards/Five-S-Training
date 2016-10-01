8d65c7fd03ce4c533f9a504811b450c93b4a4f18
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-09-30 18:41:07 -0600

    Red integration spec for user login
    User login redirects to the signup page when the 'Sign up now\!' link is clicked

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'User Login' do 
+
+  it "redirects to the signup page when the 'Sign up now!' link is clicked" do
+  	visit new_session_path
+  	click_on('Sign up!')
+  	expect(page).to have_current_path(new_user_path)
+  end
+	
+end
