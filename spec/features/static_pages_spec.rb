f13b112bb92fa8b9c9dda8b7ab03ccc0e88e7b23
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-09-30 20:50:20 -0600

    Red integration spec for login page link and title

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -5,20 +5,28 @@ RSpec.describe 'Static Pages' do
   let!(:lesson_1) { FactoryGirl.create(:lesson, title: "Lesson Factory Title 1") }
   let!(:lesson_2) { FactoryGirl.create(:lesson, title: "Lesson Factory Title 2") }
 
-  it "has a Home page, an About page, and links between the two" do
-  	visit(root_path)
-  	expect(page).to have_selector('#about')
+  it "has a Home page, an About page, a Login page, and links between the two" do
+    visit(root_path)
+    expect(page).to have_selector('#about')
     expect(page).to have_selector('#home')
+    expect(page).to have_selector('#login')
     expect(page).to have_title "5S Training | Home"
     expect(page).to have_content(lesson_1.title)
     expect(page).to have_content(lesson_2.title)
-  	click_on('About')
-  	expect(page).to have_current_path(about_path)
+    click_on('About')
+    expect(page).to have_current_path(about_path)
     expect(page).to have_selector('#about')
     expect(page).to have_selector('#home')
+    expect(page).to have_selector('#login')    
     expect(page).to have_title "5S Training | About"
-  	click_on('Home')
-  	expect(page).to have_current_path(root_path)
+    click_on('Login')
+    expect(page).to have_current_path(login_path)
+    expect(page).to have_selector('#about')
+    expect(page).to have_selector('#home')
+    expect(page).to have_selector('#login')
+    expect(page).to have_title "5S Training | Login"
+    click_on('Home')
+    expect(page).to have_current_path(root_path)
   end
-	
+  
 end
