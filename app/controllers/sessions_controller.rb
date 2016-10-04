432c3cc27c26352a37f10fadd2c1a897380bcd1a
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-10-03 20:06:55 -0600

    Green integration and controller specs for logout
    User Authentication displays a login link for users who are not logged in, and a logout link for users who are logged in. SessionsController Authentication logs the user out.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,4 +16,9 @@ include SessionsHelper
       end
   end
 
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_url
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   post 'login' => 'sessions#create'
 
+  delete 'logout' => 'sessions#destroy'
+
   get 'lessons' => 'lessons#index'
 
   resources :users, only: [:create] 
