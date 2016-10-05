addf26be5413b2b87eb83a4e97a0a1db9cb96f06
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-10-04 18:56:56 -0600

    Red controller spec for automatic login upon signup
    UsersController automatically logs in a new user upon signup.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+include SessionsHelper
 
 RSpec.describe UsersController, type: :controller do
 
@@ -7,6 +8,17 @@ RSpec.describe UsersController, type: :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
+  end
+
+  describe "Automatic Login at Signup" do
+
+  it "automatically logs in a new user upon signup" do
+    valid_user = FactoryGirl.build(:user)
+    expect(logged_in?).to be_falsy
+    post :create, user: { email: valid_user.email, password: valid_user.password, password_confirmation: valid_user.password_confirmation }    
+    expect(logged_in?).to be_truthy
+  end
+
   end
 
 end
