b25dbdbe64c401bbc6c3ae94121c3e6a8677e227
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-10-08 13:34:48 -0600

    Allow blank password when updating user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-\.]+@[\w\-\.]+\.[a-z]+\z/i	
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true
+  validates :password, presence: true, allow_nil: true
   has_secure_password
 end
