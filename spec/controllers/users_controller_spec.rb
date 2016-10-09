5e2942e407f9f9f23c9eb8add368d700b7fbae13
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-10-09 15:11:29 -0600

    Red controller spec for users#destroy
    User Deleting does not allow an unauthenticated user to send a DELETE request to users#destroy.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,9 +10,9 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
-  describe "Automatic Login at Signup" do
+  describe "Automatic Authentication at Signup" do
 
-  it "automatically logs in a new user upon signup" do
+  it "automatically authenticates a new user upon signup" do
     valid_user = FactoryGirl.build(:user)
     expect(logged_in?).to be_falsy
     post :create, user: { email: valid_user.email, password: valid_user.password, password_confirmation: valid_user.password_confirmation }    
@@ -23,12 +23,22 @@ RSpec.describe UsersController, type: :controller do
 
   describe "User Updating" do
 
-    it "requires the user to be logged in in order to send a PATCH or PUT request to users#update" do
+    it "does not allow an unauthenticated user to send a PATCH or PUT request to users#update" do
       expect(logged_in?).to be_falsy
       patch :update, { id: 1 , user: { email: "updated_email_attempt@gmail.com", password: "updated_password_attempt", password_confirmation: "updated_password_attempt" } } 
       expect(response).to redirect_to(login_path)    
       put :update, { id: 1 , user: { email: "updated_email_attempt@gmail.com", password: "updated_password_attempt", password_confirmation: "updated_password_attempt" } }
       expect(response).to redirect_to(login_path)   
+    end
+
+  end
+
+  describe "User Deleting" do
+
+    it "does not allow an unauthenticated user to send a DELETE request to users#destroy" do
+      expect(logged_in?).to be_falsy
+      delete :destroy, { id: 1 } 
+      expect(response).to redirect_to(login_path) 
     end
 
   end
