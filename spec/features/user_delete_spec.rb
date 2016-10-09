28f13367d208c75ca0966b066f552f62457f720d
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-10-09 17:28:45 -0600

    Red integration spec for deleting a user
    User Deleting allows an authenticated user to delete their profile.

---
+++ spec/features/user_delete_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "User Deleting" do
+
+  let(:user)  { FactoryGirl.create(:user) }
+	
+  it "allows an authenticated user to delete their profile" do
+  	log_in_with(user.email, user.password)
+  	user_count_before_destroy = User.count
+  	click_on('Delete Profile')
+  	expect(page).to have_current_path(confirm_deletion_path)
+  	click_on('Yes')
+    user_count_after_destroy = User.count
+    expect(user_count_after_destroy).to eql(user_count_before_destroy - 1)
+    expect(page).to have_content('deleted')
+    expect(page).to have_current_path(root_path)
+  end
+
+end
