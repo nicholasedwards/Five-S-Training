a5a4d1fc7829720f6f295f09d66a09b733942ab6
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-10-23 18:48:01 -0600

    Minor refactoring
    Extract logic from lessons index template into helper methods. Eliminate some unnecessary code.

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,2 +1,11 @@
 module LessonsHelper
+
+  def completed_lesson?(lesson)
+    lesson.users_who_completed_this_lesson.include? @current_user
+  end
+
+  def next_lesson?(i)
+    i == @current_user.completed_lessons.count
+  end
+
 end
