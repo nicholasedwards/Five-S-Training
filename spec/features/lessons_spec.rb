bbe781b1b8afea8b2a23e4e4d49105204a65ebee
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-10-27 17:50:13 -0600

    Red integration spec for lesson completions
    Lessons feature allows a user to complete a lesson, followed by a redirect to the lessons index page.

--- spec/features/lessons_spec.rb
+++ spec/features/lessons_spec.rb
@@ -35,4 +35,15 @@ RSpec.describe 'Lessons' do
     expect(page).to have_selector(".incomplete-lesson", text: "Lesson not yet completed")
   end 
 
+  it 'allows a user to complete a lesson, followed by a redirect to the lessons index page' do
+    log_in_with(user.email, user.password)
+    click_on "#{lesson_1.title}"
+    expect(page).to have_current_path(lesson_path(lesson_1))
+    click_on "I have read this lesson and I understand it!"
+    expect(page).to have_content("Great job! You have completed #{lesson_1.title}.")
+    expect(page).to have_current_path(lessons_path)
+    expect(lesson_1.users_who_completed_this_lesson).to include(user)
+    expect(user.completed_lessons).to include(lesson_1)
+  end
+
 end
