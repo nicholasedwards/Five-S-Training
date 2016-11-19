2350052cf7678dd175a071312d23a4881bd89aec
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-11-19 15:56:30 -0700

    Green unit spec for destroying dependent associations
    LessonCompletions model destroys associated lesson completions when a user or lesson is destroyed.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,6 @@
 class Lesson < ActiveRecord::Base
   validates :title, presence: true
   validates :content, presence: true
-  has_many :lesson_completions
+  has_many :lesson_completions, dependent: :destroy
   has_many :users_who_completed_this_lesson, through: :lesson_completions, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true, allow_nil: true
   has_secure_password
-  has_many :lesson_completions
+  has_many :lesson_completions, dependent: :destroy
   has_many :completed_lessons, through: :lesson_completions, source: :lesson
 end
