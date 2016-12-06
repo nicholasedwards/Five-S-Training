622348ce33fd3bc38b253b7c6dd33c40a6ba7c41
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-12-05 18:56:07 -0700

    Change landing page spec for recently removed HTML content

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,17 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'Static Pages' do 
 
-  let!(:lesson_1) { FactoryGirl.create(:lesson, title: "Lesson Factory Title 1") }
-  let!(:lesson_2) { FactoryGirl.create(:lesson, title: "Lesson Factory Title 2") }
-
   it "has a Home page, an About page, a Login page, and links between the two" do
     visit(root_path)
     expect(page).to have_selector('#about')
     expect(page).to have_selector('#home')
     expect(page).to have_selector('#login')
     expect(page).to have_title "5S Training | Home"
-    expect(page).to have_content(lesson_1.title)
-    expect(page).to have_content(lesson_2.title)
     click_on('About')
     expect(page).to have_current_path(about_path)
     expect(page).to have_selector('#about')
