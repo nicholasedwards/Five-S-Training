b355374b85a51077b15f1bb419781b487e5e0d09
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-09-08 19:20:52 -0600

    Re-write an unclear spec for better readability
    There was a spec that used some hacky error-handling logic to continue running the specs even when an error was raised. This error being raised was part of the specified behavior anyway. So, now, the spec explicitly specifies that an ActiveRecord::RecordInvalid error should be raised when the user attempts to save a duplicate lesson completion object. This approach is more readable, and it brings test coverage up to a clean 100%.

--- spec/models/lesson_completions_spec.rb
+++ spec/models/lesson_completions_spec.rb
@@ -1,20 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe LessonCompletion do 
+RSpec.describe LessonCompletion do
 
   let(:user) { FactoryGirl.create(:user) }
   let(:lesson) { FactoryGirl.create(:lesson) }
 
   it "validates for uniqueness of a lesson completion" do
     user.completed_lessons << lesson
-    begin
-      user.completed_lessons << lesson
-      break
-    rescue
-      expect(user.completed_lessons.count).to eq(1)
-    end
+    expect {user.completed_lessons << lesson}.to raise_error(ActiveRecord::RecordInvalid)
   end
-	
+
   it "destroys associated lesson completions when a user is destroyed" do
     user.completed_lessons << lesson
     user.destroy
